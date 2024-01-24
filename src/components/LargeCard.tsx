4bbed3faff161d7aae3dd1c57f016a71a3fd4043
Nicholaus Marsden <remember.marsman@gmail.com>
2024-01-23 19:41:49 -0500

    align the main cards

--- src/components/LargeCard.tsx
+++ src/components/LargeCard.tsx
@@ -15,12 +15,14 @@ const LargeCardContain = styled(Card)`
     padding: 33px 20px;
     border-radius: 10px;
     text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content:  space-between;
 `;
 const CardImage = styled(CardMedia)`
     height: 25rem;
     width: 100%;
     margin: 0 auto;
-    /* padding: 0 10rem; */
 `;
 
 const Title = styled(Typography)`
@@ -33,23 +35,24 @@ const ActionButton = styled(Button)`
     width: 100%;
     background-color: #3170B7;
     padding: 1.7rem 3rem;
+    &:hover{
+        background-color: #003E7F;
+    }
 `;
 export const LargeCard: React.FC<Props> = (props) => {
     const { Icon, title, subtopic, smallDesc, buttonDesc} = props;
     return (<LargeCardContain >
-            <CardHeader title={<Title variant="subtitle1">{title}</Title>}/>
-              <CardImage
-                image={ Icon }
-                title="green iguana"
-                />
+                <CardHeader title={<Title variant="subtitle1">{title}</Title>}/>
+                <CardImage
+                    image={ Icon }
+                    title="green iguana"
+                    />
                 <CardContent>
-                   <Typography variant="body1">{subtopic}</Typography>
-                   <Typography variant="body2">{smallDesc}</Typography>
+                <Typography variant="body1">{subtopic}</Typography>
+                <Typography variant="body2">{smallDesc}</Typography>
                 </CardContent>
                 <CardActions>
-                    <ActionButton
-
-                     variant="contained" size="large">{buttonDesc}</ActionButton>
+                    <ActionButton variant="contained" size="large">{buttonDesc}</ActionButton>
                 </CardActions>
         </LargeCardContain>
     );

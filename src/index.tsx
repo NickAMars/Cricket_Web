905a7aeaa45c1274a2cf85794f4347100f6d5c3b
Nicholaus Marsden <remember.marsman@gmail.com>
2024-02-01 06:35:37 -0500

    removing react router

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import "./global.css";
 import { App } from './app';
-import { BrowserRouter as Router } from "react-router-dom";
 import { theme } from './theme';
 import { ThemeProvider } from '@mui/material/styles';
 
@@ -11,9 +10,7 @@ const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
-                    <Router>
-                        <App />
-                    </Router>
+            <App />
         </ThemeProvider>
     </React.StrictMode>
 );
